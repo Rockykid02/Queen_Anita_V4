8e50751eb6979ca907b25788cfcd3b4a881a8ff7
Rockykid02 <144491319+Rockykid02@users.noreply.github.com>
2025-01-30 11:27:24 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 require('dotenv').config(); 
 
-global.owner = process.env.OWNER_NUMBER.split(','); 
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
+global.owner = process.env.OWNER_NUMBER.split('263780597802'); 
+global.anitav4 = process.env.BOT_NAME || "SAGE_DICKSON-MD";
 global.ownername = process.env.OWNER_NAME || "David Cyril";
-global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.packname = process.env.PACK_NAME || "Digital_Dynamo-Lab";
+global.author = process.env.AUTHOR || "Sage Dickson";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
 global.autoTyping = process.env.AUTO_TYPING === 'true'; 
